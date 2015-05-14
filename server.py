d4c8655509f7281d95e9aa421a83d22b203f2bd7
John Else <john.else@citrix.com>
2015-05-14 01:42:01 +0100

    Add a start link to the main page where relevant
    Signed-off-by: John Else <john.else@citrix.com>

--- server.py
+++ server.py
@@ -16,7 +16,9 @@ def home():
     if logged_in_user:
         return template('main', name=logged_in_user)
     else:
-        return template('message', message="Not logged in", logout_link=False)
+        return template('message', message="Not logged in",
+                        logout_link=False,
+                        start_link=False)
 
 @route('/login/:token')
 def login(token):
@@ -24,20 +26,28 @@ def login(token):
     if logged_in_user:
         return template('message',
                         message=("Already logged in as %s" % logged_in_user),
-                        logout_link=True)
+                        logout_link=True,
+                        start_link=True)
     elif USERS.has_key(token):
         user = USERS[token]
         response.set_cookie('username', user, path=COOKIE_PATH)
         return template('message',
                          message=("Logged in as %s" % user),
-                         logout_link=True)
+                         logout_link=True,
+                         start_link=True)
     else:
-        return template('message', message="Invalid token", logout_link=False)
+        return template('message',
+                        message="Invalid token",
+                        logout_link=False,
+                        start_link=False)
 
 @route('/logout')
 def logout():
     response.delete_cookie(USERNAME, path=COOKIE_PATH)
-    return template('message', message="Logged out", logout_link=False)
+    return template('message',
+                    message="Logged out",
+                    logout_link=False,
+                    start_link=False)
 
 @route('/static/<filepath:path>')
 def static(filepath):
--- views/message.tpl
+++ views/message.tpl
@@ -1,2 +1,5 @@
 % rebase('base.tpl', jquery=False, logout_link=logout_link)
         <h4>{{message}}</h4>
+% if start_link:
+        <p><a href="/">Start</a></p>
+% end
