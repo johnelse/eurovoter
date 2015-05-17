9296a2ad166e74b18233ec1b7e61f295933c8595
John Else <john.else@citrix.com>
2015-05-18 00:24:39 +0100

    Use external DB for voter lookup
    Signed-off-by: John Else <john.else@citrix.com>

--- server.py
+++ server.py
@@ -2,22 +2,32 @@
 
 import argparse
 from bottle import request, response, route, run, static_file, template
+import sqlite3
 import sys
-
-USERS = {
-    'abc': 'Bob',
-    'def': 'Fred'
-}
 
 COOKIE_PATH='/'
 TOKEN='token'
+
+db_path=""
+
+def get_voter(token):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM voters WHERE token = '%s'" % token)
+    result = cursor.fetchone()
+    conn.close()
+    if result:
+        voter_id, name, _ = result
+        return (voter_id, name)
+    else:
+        return None
 
 @route('/')
 def home():
     cookie_token = request.get_cookie(TOKEN)
     if cookie_token:
-        logged_in_user = USERS[cookie_token]
-        return template('main', name=logged_in_user)
+        _, name = get_voter(cookie_token)
+        return template('main', name=name)
     else:
         return template('message', message="Not logged in",
                         logout_link=False,
@@ -27,23 +37,25 @@ def home():
 def login(token):
     cookie_token = request.get_cookie(TOKEN)
     if cookie_token:
-        logged_in_user = USERS[cookie_token]
+        _, name = get_voter(cookie_token)
         return template('message',
-                        message=("Already logged in as %s" % logged_in_user),
+                        message=("Already logged in as %s" % name),
                         logout_link=True,
                         start_link=True)
-    elif USERS.has_key(token):
-        user = USERS[token]
-        response.set_cookie('token', token, path=COOKIE_PATH)
-        return template('message',
-                         message=("Logged in as %s" % user),
-                         logout_link=True,
-                         start_link=True)
     else:
-        return template('message',
-                        message="Invalid token",
-                        logout_link=False,
-                        start_link=False)
+        voter = get_voter(token)
+        if voter:
+            _, name = voter
+            response.set_cookie('token', token, path=COOKIE_PATH)
+            return template('message',
+                             message=("Logged in as %s" % name),
+                             logout_link=True,
+                             start_link=True)
+        else:
+            return template('message',
+                            message="Invalid token",
+                            logout_link=False,
+                            start_link=False)
 
 @route('/logout')
 def logout():
@@ -57,6 +69,10 @@ def logout():
 def static(filepath):
     return static_file(filepath, root='static/')
 
+def set_db_path(path):
+    global db_path
+    db_path = path
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--address', default='127.0.0.1',
@@ -65,5 +81,8 @@ if __name__ == "__main__":
                         help='The port on which to listen')
     parser.add_argument('-d', '--debug', action='store_true',
                         help='Run in debug mode')
+    parser.add_argument('path', type=str,
+                        help='Path to the database file')
     args = parser.parse_args(sys.argv[1:])
+    set_db_path(args.path)
     run(host=args.address, port=args.port, debug=args.debug)
