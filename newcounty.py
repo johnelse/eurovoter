067eb3a7257cc3c48a6fab787481e3181189a8e2
John Else <john.else@citrix.com>
2015-05-18 00:52:33 +0100

    Add a script to add a new country to the database
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ newcounty.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path
+import random
+import sqlite3
+import string
+import sys
+
+def new_country(path, name):
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO countries VALUES(NULL, '%s')" % name)
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str,
+                        help='Path to the database file')
+    parser.add_argument('name', type=str,
+                        help='The name of the country to add')
+    args = parser.parse_args(sys.argv[1:])
+    if not os.path.exists(args.path):
+        raise RuntimeError('Database file does not exist')
+    new_country(args.path, args.name)
