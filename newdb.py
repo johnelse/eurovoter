f156a3fef0e7723779cccd9209538246b00c4d4a
John Else <john.else@citrix.com>
2015-05-17 23:09:41 +0100

    Add a script to create a new (empty) database
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ newdb.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path
+import sqlite3
+import sys
+
+def create_db(path):
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE voters(
+                       id INTEGER PRIMARY KEY,
+                       name TEXT,
+                       token TEXT)
+                   ''')
+    cursor.execute('''CREATE TABLE countries(
+                       id INTEGER PRIMARY KEY,
+                       name TEXT)
+                   ''')
+    cursor.execute('''CREATE TABLE votes(
+                       points INTEGER,
+                       voter_id INTEGER,
+                       country_id INTEGER,
+                       FOREIGN KEY(voter_id) REFERENCES voters(id),
+                       FOREIGN KEY(country_id) REFERENCES countries(id))
+                   ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str,
+                        help='Path at which the database should be created')
+    args = parser.parse_args(sys.argv[1:])
+    if os.path.exists(args.path):
+        raise RuntimeError('File already exists')
+    create_db(args.path)
